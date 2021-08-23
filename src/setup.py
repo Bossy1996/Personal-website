11daafc946166acb66d0109ae9b820820b6a3577
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-08-23 06:40:01 +0200

    setup file

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Personale-web',
+    version='0.1',
+    description='Flask personal website',
+    author='Jon Andoni Galdos',
+    author_email='jonandonigaldos1996@gmail.com',
+    url='placehorlder',
+    packages=['flaskr'],
+)
